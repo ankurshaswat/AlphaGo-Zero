e307378994c108cae44a6059573993d1d5337957
Sushant Rathi <rathisushant5@gmail.com>
2019-11-05 23:51:00 +0530

    Added Arena.py

---
+++ code/test_arena.py
@@ -0,0 +1,31 @@
+import random 
+from go_game import GoGame
+import numpy
+import sys
+numpy.set_printoptions(threshold=sys.maxsize)
+
+if __name__ == "__main__":
+    game = GoGame(13,5.5)
+
+    board = game.getInitBoard()
+    player = -1
+
+    for i in range(1000):
+        actions = game.getValidMoves(board,player)
+        # print(actions)
+        selected_action = None
+        possible_actions = []
+        for action , indicator in enumerate(actions):
+            if indicator == 1:
+                # selected_action = action
+                # break
+                possible_actions.append(action)
+
+        selected_action = random.choice(possible_actions)        
+        # print(selected_action)
+        # print(board)
+        # print(board.board.get_legal_coords(1))
+        # print(board.board.get_legal_coords(2))
+        (board,player) = game.getNextState(board,player,selected_action)
+
+        # print(game.get_numpy_rep(board))
