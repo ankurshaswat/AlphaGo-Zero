50732b25262ac9c98253f5b3e987c914d9812ece
udit01 <uditjainiitd@gmail.com>
2019-11-10 07:03:18 +0530

    making minmax player

---
+++ code_v2/player1.py
@@ -0,0 +1,31 @@
+import os
+import random
+from go_game import GoGame
+from NNet import NetTrainer
+from play import compete
+from utils import parse_args
+
+if __name__ == "__main__":
+    #  game = GoGame(13,5.5)
+
+    # board = game.getInitBoard()
+    # player = -1
+
+    # for i in range(1000):
+    #     actions = game.getValidMoves(board,player)
+    #     # print(actions)
+    #     selected_action = None
+    #     possible_actions = []
+    #     for action , indicator in enumerate(actions):
+    #         if indicator == 1:
+    #             # selected_action = action
+    #             # break
+    #             possible_actions.append(action)
+
+    #     selected_action = random.choice(possible_actions)        
+    #     print(selected_action)
+    #     print(board)
+    #     print(board.board.get_legal_coords(1))
+    #     print(board.board.get_legal_coords(2))
+    #     (board,player) = game.getNextState(board,player,selected_action)
+
